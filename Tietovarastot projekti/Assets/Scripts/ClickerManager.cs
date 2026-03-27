92cee119af799e80bd604303b9c32e9f93ca38e6
Ilo222 <s2401208@edu.bc.fi>
2026-03-27 12:26:20 +0200

    ClickerManager tehtävä
    Clicker peli tehtävä

---
+++ Tietovarastot projekti/Assets/Scripts/ClickerManager.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ClickerManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text highscoreText;
+
+    private int score = 0;
+
+    void Start()
+    {
+        int highscore = PlayerPrefs.GetInt("HighScore", 0);
+        highscoreText.text = "Highscore: " + highscore;
+        UpdateScoreText();
+    }
+
+    public void Click()
+    {
+        score++;
+        UpdateScoreText();
+    }
+
+    public void EndGame()  // ✅ tässä, luokan sisällä, muiden metodien ulkopuolella
+    {
+        int highscore = PlayerPrefs.GetInt("HighScore", 0);
+
+        if (score > highscore)
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            highscoreText.text = "Highscore: " + score;
+            Debug.Log("Uusi highscore: " + score);
+        }
+
+        score = 0;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
